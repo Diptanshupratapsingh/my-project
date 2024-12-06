c34b8eb219eb1df7aab832eaabd396014c66df38
Diptanshupratapsingh <154540240+Diptanshupratapsingh@users.noreply.github.com>
2024-12-06 12:43:26 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,8 @@
+public class add {
+    public static void main(String[] args) {
+        System.out.println("hello Incapp\n");
+        int a =10,b=20;
+        int r = a+b;
+        System.out.println(r);
+    }
+}
